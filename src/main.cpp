8b148b209f5f0619d944fe0c12328a739d20658a
Aaron Siegel <siegelaaron94@gmail.com>
2015-02-17 03:06:43 -0800

    used the opengl shader and program class in main,

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#include <math/mat4.hpp>
 #include <system/window.hpp>
+#include <graphics/opengl/shader.hpp>
+#include <graphics/opengl/program.hpp>
+
+#include <iostream>
 
 int main(int argc, char const *argv[]) {
     sigmafive::system::context_attributes context_attributes;
@@ -15,6 +20,30 @@ int main(int argc, char const *argv[]) {
     context_attributes.core_profile = false;
     context_attributes.vsync = false;
     sigmafive::system::window window("Sigma Five", int2(800, 600), context_attributes);
+
+    float4x4 projection_matrix = float4x4::perspective(deg_to_rad(45.0f),800.0f/600.0f,0.01,1000.0f);
+    float4x4 view_matrix;
+
+    sigmafive::graphics::opengl::shader vertex_shader(sigmafive::graphics::opengl::shader::vertex_shader);
+    vertex_shader.set_source(GLSL_440(
+        in mat4 projection_matrix;
+        in mat4 view_matrix;
+        void main() {
+        }
+    ));
+
+
+    sigmafive::graphics::opengl::shader fragment_shader(sigmafive::graphics::opengl::shader::fragment_shader);
+    fragment_shader.set_source(std::string(GLSL_440(
+        void main() {
+        }
+    )));
+
+    sigmafive::graphics::opengl::program program;
+    program.attach(vertex_shader);
+    program.attach(fragment_shader);
+
+    std::cout << program.link() << std::endl;
 
     while(window.good()) {
     }
