1c99437d90e1a7a0013e77554441cca0c9e7c27e
Aaron Siegel <siegelaaron94@gmail.com>
2015-02-17 01:47:16 -0800

    added resource_manager class.

---
+++ include/system/resource_manager.hpp
@@ -0,0 +1,44 @@
+#ifndef SIGMAFIVE_SYSTEM_RESOURCE_MANAGER_HPP
+#define SIGMAFIVE_SYSTEM_RESOURCE_MANAGER_HPP
+
+#include <system/resource.hpp>
+
+#include <unordered_map>
+#include <boost/uuid/uuid.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/functional/hash.hpp>
+
+namespace sigmafive {
+	namespace system {
+		class resource_manager {
+        public:
+            resource_manager();
+
+            virtual ~resource_manager();
+
+            resource_manager(const resource_manager &) = delete;
+
+            resource_manager(resource_manager &&) = delete;
+
+            resource_manager&operator=(const resource_manager &) = delete;
+
+            resource_manager&operator=(resource_manager &&) = delete;
+
+            boost::uuids::uuid generate_key() const;
+
+            void insert(boost::uuids::uuid resource_key,boost::shared_ptr<resource> resource);
+
+            boost::shared_ptr<resource> get(boost::uuids::uuid resource_key);
+
+            template<class T>
+            boost::shared_ptr<T> get(boost::uuids::uuid resource_key) {
+                return boost::dynamic_pointer_cast<T>(get(resource_key));
+            }
+        private:
+            std::unordered_map<boost::uuids::uuid,boost::shared_ptr<resource>,boost::hash<boost::uuids::uuid>> resources_;
+        };
+	}
+}
+
+#endif //SIGMAFIVE_SYSTEM_RESOURCE_MANAGER_HPP
---
+++ src/system/resource_manager.cpp
@@ -0,0 +1,30 @@
+#include <system/resource_manager.hpp>
+
+#include <boost/uuid/uuid_generators.hpp>
+
+namespace sigmafive {
+	namespace system {
+        resource_manager::resource_manager() {
+        }
+
+        resource_manager::~resource_manager() {
+        }
+
+        boost::uuids::uuid resource_manager::generate_key() const {
+            static boost::uuids::random_generator generator;
+            return generator();
+        }
+
+        void resource_manager::insert(boost::uuids::uuid resource_key,boost::shared_ptr<resource> resource) {
+            resources_[resource_key] = resource;
+            //TODO write to file
+        }
+
+        boost::shared_ptr<resource> resource_manager::get(boost::uuids::uuid resource_key) {
+            auto it = resources_.find(resource_key);
+            if(it != resources_.end())
+                return resources_[resource_key];
+            //TODO read from file
+        }
+    }
+}
