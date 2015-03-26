afc8214c9b537507c671497e3ecc94b8dee172b6
Aaron Siegel <siegelaaron94@gmail.com>
2015-03-26 11:41:51 -0700

    added resource manager to opengl resource_renderer.

--- engine/include/graphics/opengl/scene_renderer.hpp
+++ engine/include/graphics/opengl/scene_renderer.hpp
@@ -3,14 +3,18 @@
 
 #include <math/mat4.hpp>
 #include <game/scene.hpp>
+#include <system/resource_manager.hpp>
 
 namespace sigmafive {
 	namespace graphics {
 		namespace opengl {
 			class scene_renderer {
 			public:
+				scene_renderer(system::resource_manager &resource_ranager);
+
 				void render(float4x4 view_matrix,const game::scene &scene);
 			private:
+				system::resource_manager &resource_manager_;
 			};
 		}
 	}
--- engine/src/graphics/opengl/scene_renderer.cpp
+++ engine/src/graphics/opengl/scene_renderer.cpp
@@ -3,6 +3,10 @@
 namespace sigmafive {
 	namespace graphics {
 		namespace opengl {
+			scene_renderer::scene_renderer(system::resource_manager &resource_ranager)
+				: resource_manager_(resource_ranager) {
+			}
+
 			void scene_renderer::render(float4x4 view_matrix,const game::scene &scene) {
 			}
 		}
