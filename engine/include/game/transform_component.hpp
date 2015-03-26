5645ba3d6ebebac5bed8e483f311738daa097896
Aaron Siegel <siegelaaron94@gmail.com>
2015-03-26 12:09:35 -0700

    added transform_component.

---
+++ engine/include/game/transform_component.hpp
@@ -0,0 +1,29 @@
+#ifndef SIGMAFIVE_GAME_TRANSFORM_COMPONENT_HPP
+#define SIGMAFIVE_GAME_TRANSFORM_COMPONENT_HPP
+
+#include <math/vec3.hpp>
+#include <math/quaternion.hpp>
+#include <game/component.hpp>
+
+namespace sigmafive {
+	namespace game {
+		class transform_component : public game::component {
+			SIGMAFIVE_COMPONENT();
+		public:
+			float3 position;
+			float3 scale;
+			quaternionf rotation;
+		private:
+			friend class boost::serialization::access;
+			template <typename Archive>
+			void serialize(Archive& ar, const unsigned int version) {
+				ar & boost::serialization::base_object<game::component>(*this);
+				ar & position;
+				ar & scale;
+				ar & rotation;
+			}
+		};
+	}
+}
+
+#endif //SIGMAFIVE_GAME_TRANSFORM_COMPONENT_HPP
---
+++ engine/src/game/transform_component.cpp
@@ -0,0 +1,8 @@
+#include <game/transform_component.hpp>
+
+namespace sigmafive {
+	namespace game {
+	}
+}
+
+SIGMAFIVE_IMPLEMENT_COMPONENT(sigmafive::game::transform_component);
