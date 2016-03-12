925b74db48fb39f1d27e8dd6c9796115cee41bd2
Aaron Siegel <siegelaaron94@gmail.com>
2016-03-11 23:26:15 -0800

    added a viewport.

--- include/sigmafive/graphics/context.hpp
+++ include/sigmafive/graphics/context.hpp
@@ -5,9 +5,24 @@
 
 #include <glm/mat4x4.hpp>
 #include <glm/vec2.hpp>
+#include <sigmafive/transform.hpp>
+#include <sigmafive/graphics/static_mesh_instance.hpp>
 
 namespace sigmafive {
 namespace graphics {
+//    // PSEUDO code for occlusion query culling
+//    auto software_occlusion_cull(depthbuff,occluders_begin,occluders_end,instances_begin,instances_end) {
+//        std::for_each(occluders_begin,occluders_end,cpu_render(depthbuff,_1));
+//        return std::stable_partition(instances_begin,instances_end,cpu_render(depthbuff,_1));
+//    }
+
+    struct view_port {
+        transform_manager &transforms;
+        static_mesh_instance_manager &static_mesh_instances;
+        glm::mat4 projection_matrix;
+        glm::mat4 view_matrix;
+    };
+
     class SIGMAFIVE_API context {
     public:
         context();
@@ -16,7 +31,21 @@ namespace graphics {
 
         virtual void resize(glm::uvec2 size) = 0;
 
-        virtual void render(glm::mat4 projection, glm::mat4 view) = 0;
+//        // PSEUDO code for occlusion culling
+//        virtual void render(const view_port &viewport) {
+//            if(viewport.dirty)
+//                octree.update(viewport,viewport.static_mesh_instances.begin(),viewport.static_mesh_instances.end());
+//            else
+//                octree.update(viewport,viewport.static_mesh_instances.dirty_begin(),viewport.static_mesh_instances.dirty_end());
+
+//            sorted.resize(octree.visable_size());
+//            std::copy(octree.visable_begin(),octree.visable_end());
+//            std::stable_sort(sorted.begin(),sorted.end(),...);
+//            auto end = graphics::software_occlusion_cull(occuders.begin(),occuders.end(),sorted.begin(),sorted.end());
+//            render(viewport,sorted.begin(),sorted.end());
+//        }
+
+        virtual void render(const view_port &viewport) = 0;
 
     private:
     };
