73bf5bde51a670e125a35e4b1cf2c40ba30c31d4
Aaron Siegel <siegelaaron94@gmail.com>
2017-01-12 01:10:17 -0800

    Added glslang validation.

--- tools/scompiler/src/shader_compiler.cpp
+++ tools/scompiler/src/shader_compiler.cpp
@@ -3,6 +3,8 @@
 
 #include <sigma/graphics/shader.hpp>
 
+#include "../glslang/glslang/Public/ShaderLang.h"
+
 #pragma warning(push, 0)
 #include <boost/wave.hpp>
 #include <boost/wave/cpplexer/cpp_lex_iterator.hpp>
@@ -12,8 +14,107 @@
 #include <boost/archive/binary_oarchive.hpp>
 
 #include <unordered_map>
+#include <regex>
+#include <string>
 
 namespace sigma {
+const TBuiltInResource default_resources = {
+    /* .MaxLights = */ 32,
+    /* .MaxClipPlanes = */ 6,
+    /* .MaxTextureUnits = */ 32,
+    /* .MaxTextureCoords = */ 32,
+    /* .MaxVertexAttribs = */ 64,
+    /* .MaxVertexUniformComponents = */ 4096,
+    /* .MaxVaryingFloats = */ 64,
+    /* .MaxVertexTextureImageUnits = */ 32,
+    /* .MaxCombinedTextureImageUnits = */ 80,
+    /* .MaxTextureImageUnits = */ 32,
+    /* .MaxFragmentUniformComponents = */ 4096,
+    /* .MaxDrawBuffers = */ 32,
+    /* .MaxVertexUniformVectors = */ 128,
+    /* .MaxVaryingVectors = */ 8,
+    /* .MaxFragmentUniformVectors = */ 16,
+    /* .MaxVertexOutputVectors = */ 16,
+    /* .MaxFragmentInputVectors = */ 15,
+    /* .MinProgramTexelOffset = */ -8,
+    /* .MaxProgramTexelOffset = */ 7,
+    /* .MaxClipDistances = */ 8,
+    /* .MaxComputeWorkGroupCountX = */ 65535,
+    /* .MaxComputeWorkGroupCountY = */ 65535,
+    /* .MaxComputeWorkGroupCountZ = */ 65535,
+    /* .MaxComputeWorkGroupSizeX = */ 1024,
+    /* .MaxComputeWorkGroupSizeY = */ 1024,
+    /* .MaxComputeWorkGroupSizeZ = */ 64,
+    /* .MaxComputeUniformComponents = */ 1024,
+    /* .MaxComputeTextureImageUnits = */ 16,
+    /* .MaxComputeImageUniforms = */ 8,
+    /* .MaxComputeAtomicCounters = */ 8,
+    /* .MaxComputeAtomicCounterBuffers = */ 1,
+    /* .MaxVaryingComponents = */ 60,
+    /* .MaxVertexOutputComponents = */ 64,
+    /* .MaxGeometryInputComponents = */ 64,
+    /* .MaxGeometryOutputComponents = */ 128,
+    /* .MaxFragmentInputComponents = */ 128,
+    /* .MaxImageUnits = */ 8,
+    /* .MaxCombinedImageUnitsAndFragmentOutputs = */ 8,
+    /* .MaxCombinedShaderOutputResources = */ 8,
+    /* .MaxImageSamples = */ 0,
+    /* .MaxVertexImageUniforms = */ 0,
+    /* .MaxTessControlImageUniforms = */ 0,
+    /* .MaxTessEvaluationImageUniforms = */ 0,
+    /* .MaxGeometryImageUniforms = */ 0,
+    /* .MaxFragmentImageUniforms = */ 8,
+    /* .MaxCombinedImageUniforms = */ 8,
+    /* .MaxGeometryTextureImageUnits = */ 16,
+    /* .MaxGeometryOutputVertices = */ 256,
+    /* .MaxGeometryTotalOutputComponents = */ 1024,
+    /* .MaxGeometryUniformComponents = */ 1024,
+    /* .MaxGeometryVaryingComponents = */ 64,
+    /* .MaxTessControlInputComponents = */ 128,
+    /* .MaxTessControlOutputComponents = */ 128,
+    /* .MaxTessControlTextureImageUnits = */ 16,
+    /* .MaxTessControlUniformComponents = */ 1024,
+    /* .MaxTessControlTotalOutputComponents = */ 4096,
+    /* .MaxTessEvaluationInputComponents = */ 128,
+    /* .MaxTessEvaluationOutputComponents = */ 128,
+    /* .MaxTessEvaluationTextureImageUnits = */ 16,
+    /* .MaxTessEvaluationUniformComponents = */ 1024,
+    /* .MaxTessPatchComponents = */ 120,
+    /* .MaxPatchVertices = */ 32,
+    /* .MaxTessGenLevel = */ 64,
+    /* .MaxViewports = */ 16,
+    /* .MaxVertexAtomicCounters = */ 0,
+    /* .MaxTessControlAtomicCounters = */ 0,
+    /* .MaxTessEvaluationAtomicCounters = */ 0,
+    /* .MaxGeometryAtomicCounters = */ 0,
+    /* .MaxFragmentAtomicCounters = */ 8,
+    /* .MaxCombinedAtomicCounters = */ 8,
+    /* .MaxAtomicCounterBindings = */ 1,
+    /* .MaxVertexAtomicCounterBuffers = */ 0,
+    /* .MaxTessControlAtomicCounterBuffers = */ 0,
+    /* .MaxTessEvaluationAtomicCounterBuffers = */ 0,
+    /* .MaxGeometryAtomicCounterBuffers = */ 0,
+    /* .MaxFragmentAtomicCounterBuffers = */ 1,
+    /* .MaxCombinedAtomicCounterBuffers = */ 1,
+    /* .MaxAtomicCounterBufferSize = */ 16384,
+    /* .MaxTransformFeedbackBuffers = */ 4,
+    /* .MaxTransformFeedbackInterleavedComponents = */ 64,
+    /* .MaxCullDistances = */ 8,
+    /* .MaxCombinedClipAndCullDistances = */ 8,
+    /* .MaxSamples = */ 4,
+    /* .limits = */ {
+        /* .nonInductiveForLoops = */ 1,
+        /* .whileLoops = */ 1,
+        /* .doWhileLoops = */ 1,
+        /* .generalUniformIndexing = */ 1,
+        /* .generalAttributeMatrixVectorIndexing = */ 1,
+        /* .generalVaryingIndexing = */ 1,
+        /* .generalSamplerIndexing = */ 1,
+        /* .generalVariableIndexing = */ 1,
+        /* .generalConstantMatrixVectorIndexing = */ 1,
+    }
+};
+
 class glsl_preprocessing_hooks : public boost::wave::context_policies::default_preprocessing_hooks {
 public:
     std::vector<std::string> source_files;
@@ -42,10 +143,17 @@ public:
         std::string file_name{ position.get_file().c_str() };
 
         auto it = std::find(source_files.begin(), source_files.end(), file_name);
-        auto line = "#line " + std::to_string(position.get_line()) + " " + std::to_string(std::distance(source_files.begin(), source_files.end())) + "\n";
+		std::size_t file_index = 0;
+		if (it != source_files.end()) {
+			file_index = std::distance(it, source_files.end());
+		}
+		else {
+			file_index = source_files.size();
+			source_files.push_back(position.get_file().c_str());
+		}
+
+        auto line = "#line " + std::to_string(position.get_line()) + " " + std::to_string(file_index) + "\n";
         pending.push_back(boost::wave::cpplexer::lex_token<>(boost::wave::T_PP_LINE, line.c_str(), position));
-        if (it == source_files.end())
-            source_files.push_back(position.get_file().c_str());
 
         return true;
     }
@@ -65,6 +173,7 @@ bool is_shader(boost::filesystem::path file)
 
 bool compile_shaders(boost::filesystem::path outputdir, std::vector<boost::filesystem::path> include_dirs, std::vector<boost::filesystem::path> shaders)
 {
+    glslang::InitializeProcess();
     bool all_good = true;
     for (const auto& file_path : shaders) {
         // TODO account for #include changes
@@ -89,6 +198,7 @@ bool compile_shaders(boost::filesystem::path outputdir, std::vector<boost::files
 
             ctx.add_macro_definition("SIGMA_ENGINE_SHADER");
 
+            EShLanguage glslLangType;
             sigma::graphics::shader_data shader;
             shader.type = ext_to_type[file_path.extension().string()];
             sigma::resource::development_identifier rid;
@@ -97,15 +207,18 @@ bool compile_shaders(boost::filesystem::path outputdir, std::vector<boost::files
             case graphics::shader_type::vertex: {
                 ctx.add_macro_definition("SIGMA_ENGINE_VERTEX_SHADER");
                 rid = sigma::resource::development_identifier{ "vertex", file_path };
+                glslLangType = EShLangVertex;
                 break;
             }
             case graphics::shader_type::fragment: {
                 ctx.add_macro_definition("SIGMA_ENGINE_FRAGMENT_SHADER");
                 rid = sigma::resource::development_identifier{ "fragment", file_path };
+                glslLangType = EShLangFragment;
                 break;
             }
             case graphics::shader_type::geometry: {
                 ctx.add_macro_definition("SIGMA_ENGINE_GEOMETRY_SHADER");
+                glslLangType = EShLangGeometry;
                 rid = sigma::resource::development_identifier{ "geometry", file_path };
                 break;
             }
@@ -130,6 +243,34 @@ bool compile_shaders(boost::filesystem::path outputdir, std::vector<boost::files
                 ++first;
             }
 
+            glslang::TShader glslShader{ glslLangType };
+            const char* src = shader.source.c_str();
+            glslShader.setStrings(&src, 1);
+
+			if (!glslShader.parse(&default_resources, 330, true, EShMsgDefault)) {
+				
+
+				std::string error_message = glslShader.getInfoLog();
+				//std::cout << "<<< " << error_message << " >>>" << std::endl;
+
+				std::regex error_regex("error\\s*:\\s*(\\d+):(\\d+):(.*)", std::regex_constants::icase);
+				std::smatch res;
+
+				const auto begin_iterator = std::sregex_iterator(error_message.cbegin(), error_message.cend(), error_regex);
+				const auto end_iterator = std::sregex_iterator();
+
+				for (auto it = begin_iterator; it != end_iterator; ++it)
+				{
+					auto match = *it;
+					std::cerr << ctx.get_hooks().source_files[std::stoi(match[1])] << "(" << match[2] << "): error :" << match[3] << std::endl; // MSVC output
+				}
+
+				all_good = false;
+				continue;
+			}
+
+		
+
             auto final_path = outputdir / std::to_string(rid.value());
             std::ofstream stream{ final_path.string(), std::ios::binary | std::ios::out };
             boost::archive::binary_oarchive oa(stream);
@@ -149,6 +290,7 @@ bool compile_shaders(boost::filesystem::path outputdir, std::vector<boost::files
                       << "unexpected exception caught." << std::endl;
         }
     }
+    glslang::FinalizeProcess();
     return all_good;
 }
 }
