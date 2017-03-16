9db6552d31db18a4acdcb638fa4d6f9d6e05d268
Aaron Siegel <siegelaaron94@gmail.com>
2017-03-15 21:52:20 -0700

    Revert "Refactored component storage into component sets."
    This reverts commit 53c9f4c720f13888cc2f3762dfb9e7db9bac9eb6.

--- include/sigma/world.hpp
+++ include/sigma/world.hpp
@@ -10,6 +10,26 @@
 #include <vector>
 
 namespace sigma {
+template <class... Components>
+struct component_set {
+    using component_mask_type = std::bitset<sizeof...(Components)>;
+
+    template <class Component>
+    static size_t index_of()
+    {
+        return index<Component, Components...>::value;
+    }
+
+    template <class... SubComponents>
+    static component_mask_type mask_for()
+    {
+        component_mask_type m;
+        auto l = { m |= (1 << index_of<SubComponents>())... };
+        (void)l;
+        return m;
+    }
+};
+
 struct component_storage {
     std::vector<char*> chunks;
     size_t count_per_chunk = 256;
@@ -62,84 +82,9 @@ struct component_storage {
 };
 
 template <class... Components>
-struct component_set {
-    using component_mask_type = std::bitset<sizeof...(Components)>;
-
-    template <class Component>
-    static size_t index_of()
-    {
-        return index<Component, Components...>::value;
-    }
-
-    template <class... SubComponents>
-    static component_mask_type mask_for()
-    {
-        component_mask_type m;
-        auto l = { m |= (1 << index_of<SubComponents>())... };
-        (void)l;
-        return m;
-    }
-
-    template <class Component, class... Arguments>
-    Component* add(entity e, Arguments&&... args)
-    {
-        if (e.index >= entity_masks.size())
-            entity_masks.resize(e.index + 1);
-
-        entity_masks[e.index] |= component_mask_type(1 << index_of<Component>());
-
-        component_storage& data = component_data[index_of<Component>()];
-        return data.template add<Component>(e.index, std::forward<Arguments>(args)...);
-    }
-
-    template <class Component>
-    bool has(entity e) const
-    {
-        auto comp_mask = mask_for<Component>();
-        return e.index < entity_masks.size() && ((entity_masks[e.index] & comp_mask) == comp_mask);
-    }
-
-    template <class... SubComponents>
-    void update_mask(entity e, std::bitset<sizeof...(SubComponents)>& mask) const
-    {
-        if (e.index < entity_masks.size()) {
-            auto l = { (mask[component_set<SubComponents...>::template index_of<SubComponents>()] = entity_masks[e.index][index_of<SubComponents>()])... };
-            (void)l;
-        }
-    }
-
-    template <class Component>
-    Component* get(entity e)
-    {
-        assert(e.index < entity_masks.size());
-        assert(has<Component>(e));
-
-        component_storage& data = component_data[index_of<Component>()];
-        return data.template get<Component>(e.index);
-    }
-
-    template <class Component>
-    void remove(entity e)
-    {
-        if (has<Component>(e)) {
-            entity_masks[e.index] &= ~component_mask_type(1 << index_of<Component>());
-            component_storage& data = component_data[index_of<Component>()];
-            data.template remove<Component>(e.index);
-        }
-    }
-
-    void remove_components(entity e)
-    {
-        using expand_type = int[];
-        expand_type{ (remove<Components>(e), 0)... };
-    }
-
-    std::vector<component_mask_type> entity_masks;
-    std::array<component_storage, sizeof...(Components)> component_data;
-};
-
-template <class... Components>
-struct world : public component_set<Components...> {
+struct world {
+    using component_set_type = component_set<Components...>;
+    using component_mask_type = typename component_set<Components...>::component_mask_type;
 
     world() = default;
     world(world<Components...>&&) = default;
@@ -150,12 +95,14 @@ struct world : public component_set<Components...> {
         if (free_entities.empty()) {
             entity e{ std::uint32_t(entities.size()), 0 };
             entities.push_back(e);
+            entity_masks.push_back(0);
             return e;
         } else {
             auto index = free_entities.back();
             free_entities.pop_back();
             entities[index].index = index;
             entities[index].version++;
+            entity_masks[index] = 0;
             return entities[index];
         }
     }
@@ -181,7 +128,10 @@ struct world : public component_set<Components...> {
         assert(is_alive(e));
         assert(!has<Component>(e));
 
-        return component_set<Components...>::template add<Component>(e, std::forward<Arguments>(args)...);
+        entity_masks[e.index] |= component_mask_type(1 << component_set_type::template index_of<Component>());
+
+        component_storage& data = component_data[component_set_type::template index_of<Component>()];
+        return data.template add<Component>(e.index, std::forward<Arguments>(args)...);
     }
 
     template <class Component>
@@ -189,7 +139,8 @@ struct world : public component_set<Components...> {
     {
         if (!is_alive(e))
             return false;
-        return component_set<Components...>::template has<Component>(e);
+        auto comp_mask = component_set_type::template mask_for<Component>();
+        return (entity_masks[e.index] & comp_mask) == comp_mask;
     }
 
     template <class Component>
@@ -197,7 +148,8 @@ struct world : public component_set<Components...> {
     {
         assert(is_alive(e));
         assert(has<Component>(e));
-        return component_set<Components...>::template get<Component>(e);
+        component_storage& data = component_data[component_set_type::template index_of<Component>()];
+        return data.template get<Component>(e.index);
     }
 
     template <class Component>
@@ -205,13 +157,16 @@ struct world : public component_set<Components...> {
     {
         assert(is_alive(e));
         if (has<Component>(e)) {
-            component_set<Components...>::template remove<Component>(e);
+            entity_masks[e.index] &= ~component_mask_type(1 << component_set_type::template index_of<Component>());
+            component_storage& data = component_data[component_set_type::template index_of<Component>()];
+            data.template remove<Component>(e.index);
         }
     }
 
     void remove_components(entity e)
     {
-        component_set<Components...>::remove_components(e);
+        using expand_type = int[];
+        expand_type{ (world::template remove<Components>(e), 0)... };
     }
 
     void destroy(entity e)
@@ -223,23 +178,16 @@ struct world : public component_set<Components...> {
         entities[e.index].index = std::uint32_t(-1);
     }
 
-    template <class... SubComponents>
-    bool has_all(entity e) const
-    {
-        std::bitset<sizeof...(SubComponents)> mask;
-        component_set<Components...>::template update_mask<SubComponents...>(e, mask);
-        return mask.all();
-    }
-
     template <class... SubComponents, class F>
     void for_each(F f)
     {
         // TODO this still loops over entities that are dead.
+        auto mask = component_set_type::template mask_for<SubComponents...>();
         auto count = entities.size();
         for (std::size_t i = 0; i < count; ++i) {
             auto e = entities[i];
-            if (is_alive(e) && has_all<SubComponents...>(e))
-                f(e, *get<SubComponents>(e)...);
+            if (is_alive(e) && (entity_masks[e.index] & mask) == mask)
+                f(e, *(SubComponents*)(component_data[component_set_type::template index_of<SubComponents>()].template get<SubComponents>(e.index))...);
         }
     }
 
@@ -249,6 +197,8 @@ private:
 
     std::vector<entity> entities;
     std::vector<std::uint32_t> free_entities;
+    std::vector<component_mask_type> entity_masks;
+    std::array<component_storage, sizeof...(Components)> component_data;
 };
 }
 
