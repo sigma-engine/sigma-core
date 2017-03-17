30492e17e6b67f8e850e3d3d2c0de8c3b9e8872e
Aaron Siegel <siegelaaron94@gmail.com>
2017-03-16 20:16:42 -0700

    Used lasote's gtest.

--- conanfile.py
+++ conanfile.py
@@ -4,22 +4,20 @@ from conans import ConanFile, CMake
 class SigmaEngineConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = (
-            "glm/0.9.7.6@dlarudgus20/stable"
-            , "Boost/1.60.0@lasote/stable"
-            , "jsoncpp/1.7.3@theirix/stable"
-            , "libpng/1.6.21@lasote/stable"
-            , "libtiff/4.0.6@bilke/stable"
-            , "libjpeg/9a@Kaosumaru/stable"
-            , "SDL2/2.0.5@lasote/stable"
-            , "glad/master@siegelaaron94/develop"
-            , "gtest/master@smspillaz/gtest"
-            , "google-benchmark/1.1.0@cpace6/stable"
-            , "assimp/3.3.1@siegelaaron94/develop"
+        "glm/0.9.7.6@dlarudgus20/stable"
+        , "Boost/1.60.0@lasote/stable"
+        , "jsoncpp/1.7.3@theirix/stable"
+        , "libpng/1.6.21@lasote/stable"
+        , "libtiff/4.0.6@bilke/stable"
+        , "libjpeg/9a@Kaosumaru/stable"
+        , "SDL2/2.0.5@lasote/stable"
+        , "glad/master@siegelaaron94/develop"
+        , "gtest/1.8.0@lasote/stable"
+        , "google-benchmark/1.1.0@cpace6/stable"
+        , "assimp/3.3.1@siegelaaron94/develop"
     )
-    # "jsoncpp/1.8.0@siegelaaron94/develop"
-    # "Boost/1.61.0@siegelaaron94/develop"
     generators = "cmake", "txt"
-    # default_options = "Boost:fPIC=True", "jsoncpp:fPIC=True"
+    default_options = "gtest:shared=False"
 
     def imports(self):
         if self.settings.os == "Windows":
