28dc4b0c4f75e495f58b2e67d63351f7f13a0da9
Aaron Siegel <siegelaaron94@gmail.com>
2017-05-15 21:00:59 -0700

    Use conan package for glslang.

--- tools/scompiler/src/shader_validation.cpp
+++ tools/scompiler/src/shader_validation.cpp
@@ -3,105 +3,12 @@
 #include <sigma/graphics/shader.hpp>
 
 #include <glslang/Public/ShaderLang.h>
+
 #include <regex>
 
-static const TBuiltInResource default_resources = {
-    32, // MaxLights
-    6, // MaxClipPlanes
-    32, // MaxTextureUnits
-    32, // MaxTextureCoords
-    64, // MaxVertexAttribs
-    4096, // MaxVertexUniformComponents
-    64, // MaxVaryingFloats
-    32, // MaxVertexTextureImageUnits
-    80, // MaxCombinedTextureImageUnits
-    32, // MaxTextureImageUnits
-    4096, // MaxFragmentUniformComponents
-    32, // MaxDrawBuffers
-    128, // MaxVertexUniformVectors
-    8, // MaxVaryingVectors
-    16, // MaxFragmentUniformVectors
-    16, // MaxVertexOutputVectors
-    15, // MaxFragmentInputVectors
-    -8, // MinProgramTexelOffset
-    7, // MaxProgramTexelOffset
-    8, // MaxClipDistances
-    65535, // MaxComputeWorkGroupCountX
-    65535, // MaxComputeWorkGroupCountY
-    65535, // MaxComputeWorkGroupCountZ
-    1024, // MaxComputeWorkGroupSizeX
-    1024, // MaxComputeWorkGroupSizeY
-    64, // MaxComputeWorkGroupSizeZ
-    1024, // MaxComputeUniformComponents
-    16, // MaxComputeTextureImageUnits
-    8, // MaxComputeImageUniforms
-    8, // MaxComputeAtomicCounters
-    1, // MaxComputeAtomicCounterBuffers
-    60, // MaxVaryingComponents
-    64, // MaxVertexOutputComponents
-    64, // MaxGeometryInputComponents
-    128, // MaxGeometryOutputComponents
-    128, // MaxFragmentInputComponents
-    8, // MaxImageUnits
-    8, // MaxCombinedImageUnitsAndFragmentOutputs
-    8, // MaxCombinedShaderOutputResources
-    0, // MaxImageSamples
-    0, // MaxVertexImageUniforms
-    0, // MaxTessControlImageUniforms
-    0, // MaxTessEvaluationImageUniforms
-    0, // MaxGeometryImageUniforms
-    8, // MaxFragmentImageUniforms
-    8, // MaxCombinedImageUniforms
-    16, // MaxGeometryTextureImageUnits
-    256, // MaxGeometryOutputVertices
-    1024, // MaxGeometryTotalOutputComponents
-    1024, // MaxGeometryUniformComponents
-    64, // MaxGeometryVaryingComponents
-    128, // MaxTessControlInputComponents
-    128, // MaxTessControlOutputComponents
-    16, // MaxTessControlTextureImageUnits
-    1024, // MaxTessControlUniformComponents
-    4096, // MaxTessControlTotalOutputComponents
-    128, // MaxTessEvaluationInputComponents
-    128, // MaxTessEvaluationOutputComponents
-    16, // MaxTessEvaluationTextureImageUnits
-    1024, // MaxTessEvaluationUniformComponents
-    120, // MaxTessPatchComponents
-    32, // MaxPatchVertices
-    64, // MaxTessGenLevel
-    16, // MaxViewports
-    0, // MaxVertexAtomicCounters
-    0, // MaxTessControlAtomicCounters
-    0, // MaxTessEvaluationAtomicCounters
-    0, // MaxGeometryAtomicCounters
-    8, // MaxFragmentAtomicCounters
-    8, // MaxCombinedAtomicCounters
-    1, // MaxAtomicCounterBindings
-    0, // MaxVertexAtomicCounterBuffers
-    0, // MaxTessControlAtomicCounterBuffers
-    0, // MaxTessEvaluationAtomicCounterBuffers
-    0, // MaxGeometryAtomicCounterBuffers
-    1, // MaxFragmentAtomicCounterBuffers
-    1, // MaxCombinedAtomicCounterBuffers
-    16384, // MaxAtomicCounterBufferSize
-    4, // MaxTransformFeedbackBuffers
-    64, // MaxTransformFeedbackInterleavedComponents
-    8, // MaxCullDistances
-    8, // MaxCombinedClipAndCullDistances
-    4, // MaxSamples
-    // limits
-    {
-        1, // nonInductiveForLoops
-        1, // whileLoops
-        1, // doWhileLoops
-        1, // generalUniformIndexing
-        1, // generalAttributeMatrixVectorIndexing
-        1, // generalVaryingIndexing
-        1, // generalSamplerIndexing
-        1, // generalVariableIndexing
-        1, // generalConstantMatrixVectorIndexing
-    }
-};
+namespace glslang {
+extern const TBuiltInResource DefaultTBuiltInResource;
+}
 
 struct glsl_process {
     glsl_process()
@@ -113,6 +20,7 @@ struct glsl_process {
         glslang::FinalizeProcess();
     }
 };
+
 bool sigma::validate_shader(const graphics::shader_data& shader, std::vector<boost::filesystem::path> source_filenames)
 {
     glsl_process validation_process;
@@ -141,7 +49,7 @@ bool sigma::validate_shader(const graphics::shader_data& shader, std::vector<boo
     const char* src = shader.source.c_str();
     glslShader.setStrings(&src, 1);
 
-    if (!glslShader.parse(&default_resources, 330, true, EShMsgDefault)) {
+    if (!glslShader.parse(&glslang::DefaultTBuiltInResource, 330, true, EShMsgDefault)) {
 
         std::string error_message = glslShader.getInfoLog();
 
