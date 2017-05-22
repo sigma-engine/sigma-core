1de63712d9cd75d153a5ab3deb83bf232dc37c74
Aaron Siegel <siegelaaron94@gmail.com>
2017-05-22 08:06:17 -0700

    Use template specialization to avoid having to include headers in a certain order.

--- include/sigma/util/std140_conversion.hpp
+++ include/sigma/util/std140_conversion.hpp
@@ -25,95 +25,118 @@ namespace std140 {
         return ((number + multiple - 1) / multiple) * multiple;
     }
 
-    // static std::ptrdiff_t to_std140(bool source,std::uint8_t* output, std::ptrdiff_t &offset)
-    // {
-    //     output = source;
-    // }
-    //
-    // static std::ptrdiff_t to_std140(int source,std::uint8_t* output, std::ptrdiff_t &offset)
-    // {
-    //     output = source;
-    // }
-    //
-    // static std::ptrdiff_t to_std140(unsigned long int source,std::uint8_t* output, std::ptrdiff_t &offset)
-    // {
-    //     output = (Json::UInt64)source;
-    // }
-
-    static std::ptrdiff_t to_std140(float source, std::uint8_t* output, std::ptrdiff_t& offset)
+    template <class T>
+    std::ptrdiff_t to_std140(const T& source, std::uint8_t* output, std::ptrdiff_t& offset)
     {
-        auto N = sizeof(float);
-        auto alignment = N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, &source, sizeof(float));
-        return offset + N;
+        return type_traits<T>::convert(source, output, offset);
     }
 
-    static std::ptrdiff_t to_std140(const glm::vec2& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 2 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec2));
-        return offset + 2 * N;
-    }
+    template <>
+    struct type_traits<float> {
+        static const constexpr std::size_t size = sizeof(float);
+        static std::ptrdiff_t convert(float source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, &source, sizeof(float));
+            return offset + N;
+        }
+    };
 
-    static std::ptrdiff_t to_std140(const glm::vec3& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 4 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec3));
-        return offset + 3 * N;
-    }
+    template <>
+    struct type_traits<glm::vec2> {
+        static const constexpr std::size_t size = sizeof(glm::vec2);
+        static std::ptrdiff_t convert(const glm::vec2& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 2 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec2));
+            return offset + 2 * N;
+        }
+    };
 
-    static std::ptrdiff_t to_std140(const glm::vec4& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 4 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec4));
-        return offset + 4 * N;
-    }
+    template <>
+    struct type_traits<glm::vec3> {
+        static const constexpr std::size_t size = sizeof(glm::vec4);
+        static std::ptrdiff_t convert(const glm::vec3& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 4 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec3));
+            return offset + 3 * N;
+        }
+    };
 
-    static std::ptrdiff_t to_std140(const glm::quat& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 4 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec4));
-        return offset + 4 * N;
-    }
+    template <>
+    struct type_traits<glm::vec4> {
+        static const constexpr std::size_t size = sizeof(glm::vec4);
+        static std::ptrdiff_t convert(const glm::vec4& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 4 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec4));
+            return offset + 4 * N;
+        }
+    };
 
-    static std::ptrdiff_t to_std140(const glm::mat2& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 4 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source[0]), sizeof(glm::vec2));
-        std::memcpy(output + offset + alignment, glm::value_ptr(source[1]), sizeof(glm::vec2));
-        return offset + 2 * 4 * N;
-    }
+    template <>
+    struct type_traits<glm::quat> {
+        static const constexpr std::size_t size = sizeof(glm::vec4);
+        static std::ptrdiff_t convert(const glm::quat& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 4 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::vec4));
+            return offset + 4 * N;
+        }
+    };
 
-    static std::ptrdiff_t to_std140(const glm::mat3& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 4 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source[0]), sizeof(glm::vec3));
-        std::memcpy(output + offset + alignment, glm::value_ptr(source[1]), sizeof(glm::vec3));
-        std::memcpy(output + offset + 2 * alignment, glm::value_ptr(source[2]), sizeof(glm::vec3));
-        return offset + 3 * 4 * N;
-    }
+    template <>
+    struct type_traits<glm::mat2> {
+        static const constexpr std::size_t size = 2 * sizeof(glm::vec4);
+        static std::ptrdiff_t convert(const glm::mat2& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 4 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source[0]), sizeof(glm::vec2));
+            std::memcpy(output + offset + alignment, glm::value_ptr(source[1]), sizeof(glm::vec2));
+            return offset + 2 * 4 * N;
+        }
+    };
 
-    static std::ptrdiff_t to_std140(const glm::mat4& source, std::uint8_t* output, std::ptrdiff_t& offset)
-    {
-        auto N = sizeof(float);
-        auto alignment = 4 * N;
-        offset = round_up(offset, alignment);
-        std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::mat4));
-        return offset + 4 * 4 * N;
-    }
+    template <>
+    struct type_traits<glm::mat3> {
+        static const constexpr std::size_t size = 3 * sizeof(glm::vec4);
+        static std::ptrdiff_t convert(const glm::mat3& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 4 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source[0]), sizeof(glm::vec3));
+            std::memcpy(output + offset + alignment, glm::value_ptr(source[1]), sizeof(glm::vec3));
+            std::memcpy(output + offset + 2 * alignment, glm::value_ptr(source[2]), sizeof(glm::vec3));
+            return offset + 3 * 4 * N;
+        }
+    };
+
+    template <>
+    struct type_traits<glm::mat4> {
+        static const constexpr std::size_t size = sizeof(glm::mat4);
+        static std::ptrdiff_t convert(const glm::mat4& source, std::uint8_t* output, std::ptrdiff_t& offset)
+        {
+            auto N = sizeof(float);
+            auto alignment = 4 * N;
+            offset = round_up(offset, alignment);
+            std::memcpy(output + offset, glm::value_ptr(source), sizeof(glm::mat4));
+            return offset + 4 * 4 * N;
+        }
+    };
 }
 }
 
