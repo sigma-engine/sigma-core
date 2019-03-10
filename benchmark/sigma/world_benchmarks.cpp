02d92ae342146cda7d7d83c127f926fa445709ad
Aaron Siegel <siegelaaron94@gmail.com>
2019-03-10 09:35:09 -0700

    Remove test component store.

--- benchmark/sigma/world_benchmarks.cpp
+++ benchmark/sigma/world_benchmarks.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 
 #include <sigma/world.hpp>
+#include <entt/entt.hpp>
 
 //#include "setup.hpp"
 
@@ -36,6 +37,13 @@ struct movement_system1 {
         });
     }
 
+    void process(entt::registry<>& registry, float dt)
+    {
+        registry.group<position, direction>().each([&](const auto, auto &pos, auto &dir) {
+            this->inner_loop(pos, dir, dt);
+        });
+    }
+
     void inner_loop(position& pos, const direction& dir, float dt) const
     {
         pos.x += dir.x * dt;
@@ -50,7 +58,7 @@ public:
     }
 };
 
-BENCHMARK_DEFINE_F(entity_system, for_each)
+BENCHMARK_DEFINE_F(entity_system, sigma)
 (benchmark::State& st)
 {
     benchmark_world w;
@@ -59,8 +67,9 @@ BENCHMARK_DEFINE_F(entity_system, for_each)
     for (int i = 0; i < st.range(0); ++i) {
         auto e = w.create();
         w.add<position>(e, 1.0f, 3.0f);
-        w.add<direction>(e, 2.0f, 3.0f);
-        w.remove<direction>(e);
+        if(i % 2 == 0) { 
+            w.add<direction>(e, 2.0f, 3.0f);
+        }
     }
 
     while (st.KeepRunning()) {
@@ -75,7 +84,38 @@ BENCHMARK_DEFINE_F(entity_system, for_each)
     st.SetComplexityN(st.range(0));
 }
 
-BENCHMARK_REGISTER_F(entity_system, for_each)
+BENCHMARK_DEFINE_F(entity_system, entt)
+(benchmark::State& st)
+{
+    entt::registry<> registry;
+    movement_system1 sys1;
+
+    for (int i = 0; i < st.range(0); ++i) {
+        auto e = registry.create();
+        registry.assign<position>(e, 1.0f, 3.0f);
+        if(i % 2 == 0) { 
+            registry.assign<direction>(e, 2.0f, 3.0f);
+        }
+    }
+
+    while (st.KeepRunning()) {
+        benchmark::DoNotOptimize(&registry);
+        sys1.process(registry, 1.0f / 60.0f);
+        // TODO there is a newer version of google benchmark that has
+        // support ClobberMemory but it is not in conan yet.
+#ifndef _MSC_VER
+        benchmark::ClobberMemory();
+#endif
+    }
+    st.SetComplexityN(st.range(0));
+}
+
+BENCHMARK_REGISTER_F(entity_system, sigma)
     ->RangeMultiplier(2)
-    ->Range(100, 1000000)
+    ->Range(100, 10000000)
+    ->Complexity();
+
+BENCHMARK_REGISTER_F(entity_system, entt)
+    ->RangeMultiplier(2)
+    ->Range(100, 10000000)
     ->Complexity();
